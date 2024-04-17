a38ea456b45b2aff11728aa8c59339faf2dc5305
Hussain Mustansir <hmustansir24@gmail.com>
2024-04-17 14:29:06 +0500

    some things done

--- Data_memory.v
+++ Data_memory.v
@@ -9,77 +9,18 @@ input [63:0] Write_Data,
 input clk, MemWrite, MemRead,
 output reg [63:0] Read_Data
 );
-reg [7:0] DataMemory [63:0];
+reg [7:0] DataMemory [255:0];
 integer i;
 initial
 begin
-for(i =0; i<64; i=i+1)begin
+for(i =0; i<256; i=i+1)begin
     DataMemory[i] = 1'b0;  // flush the data memory first
 end
-DataMemory[0] = 8'd10;
-DataMemory[1] = 8'd0;
-DataMemory[2] = 8'd0;
-DataMemory[3] = 8'd0;
-DataMemory[4] = 8'd0;
-DataMemory[5] = 8'd0;
-DataMemory[6] = 8'd0;
-DataMemory[7] = 8'd0;
-DataMemory[8] = 8'd8;
-DataMemory[9] = 8'd9;
-DataMemory[10] = 8'd10;
-DataMemory[11] = 8'd11;
-DataMemory[12] = 8'd12;
-DataMemory[13] = 8'd13;
-DataMemory[14] = 8'd14;
-DataMemory[15] = 8'd15;
-DataMemory[16] = 8'd16;
-DataMemory[17] = 8'd17;
-DataMemory[18] = 8'd18;
-DataMemory[19] = 8'd19;
-DataMemory[20] = 8'd20;
-DataMemory[21] = 8'd21;
-DataMemory[22] = 8'd22;
-DataMemory[23] = 8'd23;
-DataMemory[24] = 8'd24;
-DataMemory[25] = 8'd25;
-DataMemory[26] = 8'd26;
-DataMemory[27] = 8'd27;
-DataMemory[28] = 8'd28;
-DataMemory[29] = 8'd29;
-DataMemory[30] = 8'd30;
-DataMemory[31] = 8'd31;
-DataMemory[32] = 8'd0;
-DataMemory[33] = 8'd1;
-DataMemory[34] = 8'd2;
-DataMemory[35] = 8'd3;
-DataMemory[36] = 8'd4;
-DataMemory[37] = 8'd5;
-DataMemory[38] = 8'd6;
-DataMemory[39] = 8'd7;
-DataMemory[40] = 8'd8;
-DataMemory[41] = 8'd9;
-DataMemory[42] = 8'd0;
-DataMemory[43] = 8'd0;
-DataMemory[44] = 8'd0 ;
-DataMemory[45] = 8'd0;
-DataMemory[46] = 8'd0;
-DataMemory[47] = 8'd0;
-DataMemory[48] = 8'd0;
-DataMemory[49] = 8'd0;
-DataMemory[50] = 8'd0;
-DataMemory[51] = 8'd0;
-DataMemory[52] = 8'd0;
-DataMemory[53] = 8'd0;
-DataMemory[54] = 8'd0;
-DataMemory[55] = 8'd0;
-DataMemory[56] = 8'd0;
-DataMemory[57] = 8'd0;
-DataMemory[58] = 8'd0;
-DataMemory[59] = 8'd0;
-DataMemory[60] = 8'd0;
-DataMemory[61] = 8'd0;
-DataMemory[62] = 8'd0;
-DataMemory[63] = 8'd0;
+DataMemory[0] = 8'd3;
+DataMemory[8] = 8'd1;
+DataMemory[16] = 8'd4;
+DataMemory[24] = 8'd2;
+
 end
 always @ (*)
 begin
--- Instruction_memory.v
+++ Instruction_memory.v
@@ -9,48 +9,21 @@ reg [7:0] inst_mem [15:0];
 initial
 begin
 // store some test data in the memory 
-inst_mem[0]=8'b10000011;
-inst_mem[1]=8'b00110100;
-inst_mem[2]=8'b10000101;
-inst_mem[3]=8'b00000010;
-inst_mem[4]=8'b10110011;
-inst_mem[5]=8'b10000100;
-inst_mem[6]=8'b10011010;
-inst_mem[7]=8'b00000000;
-inst_mem[8]=8'b10010011;
-inst_mem[9]=8'b10000100;
-inst_mem[10]=8'b00010100;
-inst_mem[11]=8'b00000000;
-inst_mem[12]=8'b00100011;
-inst_mem[13]=8'b00111000;
-inst_mem[14]=8'b10010101;
-inst_mem[15]=8'b00001110;
+{inst_mem[3],inst_mem[2],inst_mem[1],inst_mem[0]} = 32'h10000513;//1
+{inst_mem[7],inst_mem[6],inst_mem[5],inst_mem[4]} = 32'h00500293;
+{inst_mem[11],inst_mem[10],inst_mem[9],inst_mem[8]} = 32'h00000b13;
+{inst_mem[15],inst_mem[14],inst_mem[13],inst_mem[12]} = 32'h00000b93;
+{inst_mem[19],inst_mem[18],inst_mem[17],inst_mem[16]} = 32'h008000ef;
+{inst_mem[23],inst_mem[22],inst_mem[21],inst_mem[20]} = 32'h04c0006f;
 
-inst_mem[16]=8'b10000011;
-inst_mem[17]=8'b00110100;
-inst_mem[18]=8'b10000101;
-inst_mem[19]=8'b00000010;
-inst_mem[20]=8'b10110011;
-inst_mem[21]=8'b10000100;
-inst_mem[22]=8'b10011010;
-inst_mem[23]=8'b00000000;
-inst_mem[23]=8'b10010011;
-inst_mem[24]=8'b10000100;
-inst_mem[25]=8'b00010100;
-inst_mem[26]=8'b00000000;
-inst_mem[27]=8'b00100011;
-inst_mem[28]=8'b00111000;
-inst_mem[29]=8'b10010101;
-inst_mem[30]=8'b00001110;
-inst_mem[31]=8'b00001110;
 
 end
 //extract the data as per the given index
 always @(Inst_Address)
 begin
-Instruction[7:0]=  inst_mem[Inst_Address+ 0 ];
-Instruction[15:8]=  inst_mem[Inst_Address+1 ];
-Instruction[23:16]= inst_mem[Inst_Address+2 ];
-Instruction[31:24]= inst_mem[Inst_Address +3];
+Instruction[7:0] = inst_mem[Inst_Address+0];
+Instruction[15:8] = inst_mem[Inst_Address+1];
+Instruction[23:16] = inst_mem[Inst_Address+2];
+Instruction[31:24] = inst_mem[Inst_Address+3];
 end
 endmodule
